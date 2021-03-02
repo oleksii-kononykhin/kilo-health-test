89375f6cf81caa98755e4acbeb371c35390e2ee8
Ieva Kaleginiene <ieva.kaleginiene@kilo.health>
2021-03-02 12:20:02 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <Welcome>Welcom to Test day at Kilo Health</Welcome>;
 }
+
+const Welcome = styled.h1`
+	margin-top: 15rem;
+	text-align: center;
+`;
 
 export default App;
