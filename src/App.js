a6efc6acd9afe6c16960cd9e7899a0b48bf55dd6
Lukas Varkalis <varkalis.lukas@gmail.com>
2021-03-02 12:54:40 +0200

    small update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
-	return <Welcome>Welcom to Test day at Kilo Health</Welcome>;
-}
+const App = () => {
+  return <Title>Welcom to Test day at Kilo Health</Title>;
+};
 
-const Welcome = styled.h1`
-	margin-top: 15rem;
-	text-align: center;
+const Title = styled.h1`
+  margin-top: 15rem;
+  text-align: center;
 `;
 
 export default App;
